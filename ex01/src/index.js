8791d980b6308c8e84907c7b6d7af5d4bf5ac430
Sue-cod <snozic892@gmail.com>
2021-03-17 00:06:15 +0100

    Hello prvom zadatku

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function myTrueBoolean() {
+    if (Boolean) {
+        return true;
+    }
+
+}
+myTrueBoolean();
+// Only change code ebove this line
+console.log(myTrueBoolean());
+module.exports = myTrueBoolean;
