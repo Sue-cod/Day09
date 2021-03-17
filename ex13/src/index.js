ee182efd8682670129c6b67008eb06d3b3e5691b
Sue-cod <snozic892@gmail.com>
2021-03-17 04:35:31 +0100

    Ovaj zadatak se moraa popraviti

---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+function inverseWhile() {
+var fiveNumbers = 5;
+while(fiveNumbers  < 0) {
+    fiveNumbers--;
+   console.log(fiveNumbers);
+}
+}
+
+
+inverseWhile();
+console.log(inverseWhile);
+module.exports = inverseWhile;
