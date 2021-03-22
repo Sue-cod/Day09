e1596c523188abcc793b46e206c0475cee7fe0a1
Sue-cod <snozic892@gmail.com>
2021-03-22 02:38:09 +0100

    izmjena za popravni

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,9 +1,16 @@
 function myDoWhile() {
-
+var i = 0;
+var myNumbers = '';
+do {
+    i !== 9 ? (myNumbers += i + ', ') : (myNumbers += i); 
+    ++i; 
+  } while (i < 10); 
+  return myNumbers; 
 }
 
-
-
-
 myDoWhile();
+console.log(myDoWhile());
 module.exports = myDoWhile;
+
+
+  
