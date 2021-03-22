38a3723d74d4858719c7e7e055e39f39cd07fda4
Sue-cod <snozic892@gmail.com>
2021-03-22 02:41:40 +0100

    izmjena za popr

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,5 +1,5 @@
 function logicalOrOperator(num) {
-    if (num > 20 || num < 30) {
+    if (num < 20 || num > 30) {
        return "Out" ;
     }
     return "In";
@@ -12,6 +12,12 @@ logicalOrOperator(23);
 logicalOrOperator(30);
 logicalOrOperator(31);
 logicalOrOperator(105);
-console.log(logicalOrOperator());
+console.log(logicalOrOperator(0));
+console.log(logicalOrOperator(9));
+console.log(logicalOrOperator(20));
+console.log(logicalOrOperator(23));
+console.log(logicalOrOperator(30));
+console.log(logicalOrOperator(31));
+console.log(logicalOrOperator(105));
 
 module.exports = logicalOrOperator;
