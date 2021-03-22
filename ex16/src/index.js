e60597e8178e85ab0b00f94b997e529a9ed94f2f
Sue-cod <snozic892@gmail.com>
2021-03-22 02:36:26 +0100

    Izmjena za popravni

--- ex16/src/index.js
+++ ex16/src/index.js
@@ -1,12 +1,14 @@
 function checkSign(num) {
-
-
+return (num > 0) ? "positive"
+: (num < 0) ? "negative"
+: (num === 0) ? "zero";
 
 }
 
-
-
-
-
-checkSign();
+checkSign(10);
+checkSign(-12);
+checkSign(0);
+console.log(checkSign(positive));
+console.log(checkSign(negative));
+console.log(checkSign(zero));
 module.exports = checkSign;
